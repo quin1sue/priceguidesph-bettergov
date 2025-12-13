31535d2ff0aaffb4bfa7090c9de70d06fb6bfe49
quin1sue <sepriothofficial@gmail.com>
2025-12-14 00:41:00 +0800

    updated sitemap

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -1,32 +1,33 @@
-import type { MetadataRoute } from "next"; 
+import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const staticUrls: MetadataRoute.Sitemap = [
+  const staticUrls: MetadataRoute.Sitemap = [
     {
       url: "https://price-guides.bettergov.ph",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1.0,
-      images: ["https://price-guides.bettergov.ph/banner-white.png"]
+      images: ["https://price-guides.bettergov.ph/banner-white.png"],
     },
     {
       url: "https://price-guides.bettergov.ph/about",
       lastModified: new Date(),
       changeFrequency: "weekly",
-      priority: 0.7
+      priority: 0.9,
     },
-    {
-      url: "https://price-guides.bettergov.ph/about",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.9
-    },
-]
-  const dynamicUrls: MetadataRoute.Sitemap = ["drug-price-index", "cigarette-index", "daily-price-index", "diesel", "currency-exchange"].map((category) => ({
+  ];
+  const dynamicUrls: MetadataRoute.Sitemap = [
+    "drug-price-index",
+    "cigarette-index",
+    "daily-price-index",
+    "diesel",
+    "indicator",
+    "currency-exchange",
+  ].map((category) => ({
     url: `https://price-guides.bettergov.ph/${category}`,
     lastModified: new Date(),
     changeFrequency: "weekly",
-    priority: 0.9
-  }))
-    return [...staticUrls, ...dynamicUrls]
+    priority: 0.9,
+  }));
+  return [...staticUrls, ...dynamicUrls];
 }
